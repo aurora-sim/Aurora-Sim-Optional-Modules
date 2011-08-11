3d46a3c7cee66914baa1295e9ca60b90da281eb4
RevolutionSmythe <asdfisbetterthanjkl_AT_gmail.com>
2011-08-10 20:53:09 -0500

    Remove linq, because it appearently matters.

--- FractalTerrain/Modules/FractalTerrain.cs
+++ FractalTerrain/Modules/FractalTerrain.cs
@@ -23,7 +23,6 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 */
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using LandscapeGenCore;
 using OpenSim.Framework;
